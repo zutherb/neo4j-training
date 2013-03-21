c85128322f0ddced8f57603cd5806cb166454f2a
zutherb <bernd.zuther@comsysto.com>
2013-03-21 16:46:51 +0100

    exercise 7

--- src/koan/java/org/neo4j/tutorial/Koan07.java
+++ src/koan/java/org/neo4j/tutorial/Koan07.java
@@ -1,9 +1,5 @@
 package org.neo4j.tutorial;
 
-import static org.junit.Assert.assertThat;
-import static org.neo4j.tutorial.matchers.ContainsOnlySpecificActors.containsOnlyActors;
-import static org.neo4j.tutorial.matchers.ContainsSpecificNumberOfNodes.containsNumberOfNodes;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,50 +10,63 @@ import org.neo4j.graphdb.traversal.Evaluation;
 import org.neo4j.graphdb.traversal.Evaluator;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.kernel.Traversal;
-import org.neo4j.kernel.Uniqueness;
+
+import static org.junit.Assert.assertThat;
+import static org.neo4j.tutorial.matchers.ContainsOnlySpecificActors.containsOnlyActors;
+import static org.neo4j.tutorial.matchers.ContainsSpecificNumberOfNodes.containsNumberOfNodes;
 
 /**
  * In this Koan we start using the new traversal framework to find interesting
  * information from the graph about the Doctor's past life.
  */
-public class Koan07
-{
+public class Koan07 {
 
     private static EmbeddedDoctorWhoUniverse universe;
 
     @BeforeClass
-    public static void createDatabase() throws Exception
-    {
-        universe = new EmbeddedDoctorWhoUniverse( new DoctorWhoUniverseGenerator() );
+    public static void createDatabase() throws Exception {
+        universe = new EmbeddedDoctorWhoUniverse(new DoctorWhoUniverseGenerator());
     }
 
     @AfterClass
-    public static void closeTheDatabase()
-    {
+    public static void closeTheDatabase() {
         universe.stop();
     }
 
     @Test
-    public void shouldDiscoverHowManyDoctorActorsHaveParticipatedInARegeneration() throws Exception
-    {
+    public void shouldDiscoverHowManyDoctorActorsHaveParticipatedInARegeneration() throws Exception {
         Node theDoctor = universe.theDoctor();
-        TraversalDescription regeneratedActors = null;
+        TraversalDescription regeneratedActors = Traversal.description()
+                .relationships(DoctorWhoRelationships.PLAYED)
+                .evaluator(new Evaluator() {
+                    @Override
+                    public Evaluation evaluate(Path propertyContainers) {
+                        if (propertyContainers.endNode().hasRelationship(DoctorWhoRelationships.REGENERATED_TO)) {
+                            return Evaluation.INCLUDE_AND_PRUNE;
+                        }
+                        return Evaluation.EXCLUDE_AND_CONTINUE;
+                    }
+                });
 
-        // YOUR CODE GOES HERE
-        // Note: every doctor has participated in a regeneration, including the first and last Doctors
-
-
-        assertThat( regeneratedActors.traverse( theDoctor ).nodes(), containsNumberOfNodes( 11 ) );
+        assertThat(regeneratedActors.traverse(theDoctor).nodes(), containsNumberOfNodes(11));
     }
 
     @Test
-    public void shouldFindTheFirstDoctor()
-    {
+    public void shouldFindTheFirstDoctor() {
         Node theDoctor = universe.theDoctor();
-        TraversalDescription firstDoctor = null;
+        TraversalDescription firstDoctor = Traversal.description()
+                .relationships(DoctorWhoRelationships.PLAYED)
+                .evaluator(new TraversalPrinter(new Evaluator() {
+                    @Override
+                    public Evaluation evaluate(Path propertyContainers) {
+                        if (propertyContainers.endNode().hasRelationship(DoctorWhoRelationships.REGENERATED_TO, Direction.OUTGOING)
+                                && !propertyContainers.endNode().hasRelationship(DoctorWhoRelationships.REGENERATED_TO, Direction.INCOMING)) {
+                            return Evaluation.INCLUDE_AND_PRUNE;
+                        }
+                        return Evaluation.EXCLUDE_AND_CONTINUE;
+                    }
+                }));
 
-        // YOUR CODE GOES HERE
-
-        assertThat( firstDoctor.traverse( theDoctor ).nodes(), containsOnlyActors( "William Hartnell" ) );
+        assertThat(firstDoctor.traverse(theDoctor).nodes(), containsOnlyActors("William Hartnell"));
     }
 }
